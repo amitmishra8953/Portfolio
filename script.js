7b1c2d482236b5dc55baa64a1e7cf9b211b91cce
Amit mishra <amitofficial451@gmail.com>
2026-01-13 15:18:53 +0530

    Initial portfolio commit

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 function scrollToContact() {
   document.getElementById("contact").scrollIntoView({
     behavior: "smooth"
@@ -6,3 +7,13 @@ function scrollToContact() {
 
 console.log("Professional Portfolio Loaded");
 
+=======
+function scrollToContact() {
+  document.getElementById("contact").scrollIntoView({
+    behavior: "smooth"
+  });
+}
+
+console.log("Professional Portfolio Loaded");
+
+>>>>>>> 6874c7e (Initial portfolio commit)
