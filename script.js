407a550c129cfb564ef75d02d8b00ad52f4342cf
Amit mishra <amitofficial451@gmail.com>
2026-01-13 17:28:31 +0530

    Fixed whitespace issue and synced local code

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function scrollToContact() {
   document.getElementById("contact").scrollIntoView({
     behavior: "smooth"
@@ -7,13 +6,3 @@ function scrollToContact() {
 
 console.log("Professional Portfolio Loaded");
 
-=======
-function scrollToContact() {
-  document.getElementById("contact").scrollIntoView({
-    behavior: "smooth"
-  });
-}
-
-console.log("Professional Portfolio Loaded");
-
->>>>>>> 6874c7e (Initial portfolio commit)
